caa8a2ce71cd5a6a89bdb13cc5915165a039e569
赵鸿飞 <zhf5256@qq.com>
2025-10-29 15:57:48 +0800

    fix: 修改配置

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '好好学习',
-  HEO_HERO_TITLE_2: '天天向上',
+  HEO_HERO_TITLE_1: '悟已往之不谏',
+  HEO_HERO_TITLE_2: '知来者之可追',
   HEO_HERO_TITLE_3: 'HAPPIER',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
