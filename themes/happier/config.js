99c0ba7c4ccb415ce2a35b89bb725eaf266aa320
赵鸿飞 <zhf5256@qq.com>
2025-10-29 17:19:58 +0800

    fix: 修改链接

--- themes/happier/config.js
+++ themes/happier/config.js
@@ -48,7 +48,7 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/happierx',
+  HEO_INFO_CARD_URL2: 'https://github.com/happier-x',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://www.happierx.top',
   HEO_INFO_CARD_TEXT3: '了解更多',
